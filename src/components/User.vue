f7c1a31b81388023e214dd5196fb9a100b2f130f
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-05-28 22:03:21 +0530

    Update user component
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,25 +1,36 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col">
+  <b-container>
+    <b-row>
+      <b-col>
         <b-button v-on:click="toggleLogin(true)" variant="primary" class="m-2">Login Tab</b-button>
         <b-button v-on:click="toggleLogin(false)" variant="primary" class="m-2">Register Tab</b-button>
-      </div>
-    </div>
+      </b-col>
+    </b-row>
     
-    <div class="row">
-      <div class="col-8 ml-auto mr-auto">
+    <b-row>
+      <b-col class="ml-auto mr-auto">
         <b-form-input v-model="name" type="text" placeholder="Enter your name" class="m-2"></b-form-input>
         <b-form-input v-model="password" type="text" placeholder="Enter your password" class="m-2"></b-form-input>
-      </div>
-    </div>
-    <br>
-    <b-button v-on:click="submitLogin" variant="success" v-if="login" id="loginBtn">Login</b-button>
-    <b-button v-on:click="submitRegister" variant="success" v-else id="registerBtn">Register</b-button>
-  </div>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <p id="errorRow" class="text-danger"></p>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-button v-on:click="submitLogin" variant="success" v-if="login" id="loginBtn">Login</b-button>
+        <b-button v-on:click="submitRegister" variant="success" v-else id="registerBtn">Register</b-button>
+      </b-col>
+    </b-row>
+  
+  </b-container>
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
     name: "User",
     data: function () {
@@ -33,8 +44,22 @@
       toggleLogin: function (login) {
         this.login = login;
       },
-      submitLogin: () => {
-        
+      submitLogin: function () {
+        let name = this.name;
+        let password = this.password
+        axios.post('http://localhost:2678/api/users/login', {
+          name: name,
+          password: password
+        }).then((response) => {
+          let data = response.data;
+          if (data.success) {
+            window.localStorage.setItem("name", name);
+            window.localStorage.setItem("password", password);
+            window.location.replace('/viewProducts')
+          } else {
+            document.getElementById('errorRow').innerText = data.msg;
+          }
+        })
       }
     }
   }
