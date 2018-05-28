e2ad9fd6917c6fb006c031596b4c6add29b46637
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-05-28 22:04:11 +0530

    Add view All Products component
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

---
+++ src/components/ViewProducts.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-container>
+    <b-row>
+      <product v-for="productItem in products" :key="productItem.id"
+      :name="productItem.name" :manufacturer="productItem.manufacturer"
+      :price="productItem.price" :productId="productItem.id"
+      ></product>
+    </b-row>
+  </b-container>
+
+</template>
+
+<script>
+  import Product from './Product'
+  import axios from 'axios'
+
+  export default {
+    name: "ViewProducts",
+    components: {
+      product: Product
+    },
+    data: function () {
+      return {
+        products: []
+      }
+    },
+    created() {
+      axios.get('http://localhost:2678/api/products').then((response) => {
+        this.products = response.data
+      })
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
