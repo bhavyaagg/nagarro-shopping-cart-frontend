75b59256d339b7811311a05c897dc9e3f3b259fd
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-05-31 01:33:16 +0530

    Product can be added
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -3,15 +3,19 @@
     <div class="row">
       <div class="col ml-auto mr-auto">
         <b-form-input v-model="name" type="text" placeholder="Enter Products name" class="m-2"></b-form-input>
-        <b-form-input v-model="manufacturer" type="text" placeholder="Enter Manufacturer's Name" class="m-2"></b-form-input>
+        <b-form-input v-model="manufacturer" type="text" placeholder="Enter Manufacturer's Name"
+                      class="m-2"></b-form-input>
         <b-form-input v-model="price" type="number" placeholder="Enter Product's Price" class="m-2"></b-form-input>
-        <button></button>
+        <b-button v-on:click="submitAdd" variant="success" id="addBtn">Add</b-button>
+        <p id="msgRow"></p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
     name: "AddProduct",
     data: function () {
@@ -21,8 +25,23 @@
         price: ''
       }
     },
-    methods: function () {
-    
+    methods: {
+      submitAdd: function () {
+        let name = this.name;
+        let manufacturer = this.manufacturer;
+        let price = this.price;
+        axios.post('http://localhost:2678/api/products/', {
+          name, manufacturer, price
+        }).then((response) => {
+          let data = response.data;
+          if (data.success) {
+            document.getElementById("msgRow").innerText = "Product Successfully Added"
+          } else {
+            document.getElementById("msgRow").innerText = "Could not add the Product"
+          }
+        })
+
+      }
     }
   }
 </script>
